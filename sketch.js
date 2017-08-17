3b373210ee76d4196378c7e32eb29e3c8ac9abce
JPrier <JoshKPrier@gmail.com>
2017-08-17 14:39:55 -0400

    Mobile Notifications

--- sketch.js
+++ sketch.js
@@ -74,6 +74,15 @@ function setup() {
 
 	if(!IsGoodPerformance) {
 		alert(ElapsedMilliseconds);
+
+		navigator.serviceWorker.register('sw.js');
+		Notification.requestPermission(function(result) {
+	  		if (result === 'granted') {
+	    		navigator.serviceWorker.ready.then(function(registration) {
+	      			registration.showNotification('Notification with ServiceWorker');
+	    		});
+  			}
+		});
 	}
 }
 
