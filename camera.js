97bbcd7639c091269687b47f160ece52147a0402
Joshua Prier <JoshKPrier@gmail.com>
2020-10-31 14:53:10 -0700

    add Camera html and js

---
+++ camera.js
@@ -0,0 +1,178 @@
+/*
+Uncomment the random lines loops and functions to see them. They are currently too much to run on a webpage
+*/
+
+
+var innerBalls = [];
+var outerBalls = [];
+//var randomLines = [];
+var innerCircleAmt = 20;
+var outerCircleAmt = 40;
+//var randomLinesAmt = 10;
+var innerRadius = 200;
+var outerRadius = 300;
+//var lineLength = 5;
+
+var ballSize = 10;
+var canSize = 1000;
+
+var indexTotal = 1000;
+var speed = .25;
+var lineDistance = 250;
+var linespeed = [1, 40];
+var spread = 25;
+
+var width;
+var height;
+
+var oL = false;
+//var iL = false;
+var spreadSpeed = .2;
+
+var colorB = 255;
+var counter = 0;
+var fps = 0;
+
+var piIndex;
+
+p5.disableFriendlyErrors = true;
+
+function setup() {
+
+	createCanvas(720, 400);
+
+	for (var i = 0; i < innerCircleAmt; i++) {
+		innerBalls[i] = new Ball();
+		innerBalls[i].create(innerRadius, i, true);
+	}
+
+	for (var i = 0; i < outerCircleAmt; i++) {
+		outerBalls[i] = new Ball();
+		outerBalls[i].create(outerRadius, i, false);
+	}
+
+	piIndex = TWO_PI / indexTotal;
+
+}
+
+function draw() {
+
+
+  	for (var i = 0; i < innerCircleAmt; i++) {
+		innerBalls[i].update();
+	}
+
+	for (var i = 0; i < outerCircleAmt; i++) {
+		outerBalls[i].update();
+
+
+		if (outerBalls[i].r > outerRadius*1.2 || oL) {
+			outerBalls[i].r -= spreadSpeed;
+
+			if (i == outerCircleAmt - 1 && outerBalls[i].r == outerRadius) {
+				oL = false;
+			} else {
+				oL = true;
+			}
+		} else {
+			outerBalls[i].r += spreadSpeed;
+		}
+	}
+
+
+}
+
+function Ball() {
+
+	this.i;
+	this.r;
+	this.x;
+	this.y;
+	this.inner;
+	this.center;
+	this.lines;
+	this.index;
+
+	this.create = function(r, i, inner) {
+
+		this.inner = inner;
+		this.r = r;
+
+		this.index = i;
+
+		if (inner) {
+			this.i = i * (indexTotal/innerCircleAmt);
+			this.center = createVector(width/2, 170);
+		} else {
+			this.i = i * (indexTotal/outerCircleAmt);
+			this.center = createVector(width/2, 170);
+		}
+
+		this.x = this.center.x + this.r * (Math.sin(piIndex * i));
+		this.y = this.center.y + this.r * (Math.cos(piIndex * i));
+
+		this.lines = 0;
+	}
+
+	this.update = function() {
+
+		if (this.inner && this.i < indexTotal) {
+			this.i += speed;
+
+		} else if (!this.inner && this.i > -indexTotal) {
+			this.i -= speed;
+
+		} else {
+			this.i = 0;
+		}
+
+		this.center.x = Math.min(width, windowWidth)/2;
+
+		this.x = this.center.x + this.r * (Math.sin(piIndex * this.i));
+		this.y = this.center.y + this.r * (Math.cos(piIndex * this.i));
+
+		this.show();
+		this.showLines();
+
+		this.lines = 0;
+	}
+
+	this.show = function() {
+		noStroke();
+		fill(colorB);
+		ellipse(this.x, this.y, ballSize, ballSize);
+	}
+
+	this.showLines = function() {
+
+		var distance;
+		var other;
+
+		noFill();
+
+		if (this.inner) {
+
+			for (var i = 0; i < outerBalls.length; i++) {
+
+				other = outerBalls[i];
+				distance = distSquared(this.x, this.y, other.x, other.y);
+
+				if (distance < lineDistance) {
+					stroke(colorB, 2 * (lineDistance-distance));
+					line(this.x, this.y, other.x, other.y);
+					this.lines++;
+				}
+
+				if (this.lines == 12) {
+					break;
+				}
+			}
+		}
+	}
+}
+
+function distSquared(x1, y1, x2, y2) {
+    var dx = x2 - x1;
+    var dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
